b9cff743962db57f0000e82888885d627af02784
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2022-07-07 18:48:18 -0300

    done

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import filedialog
 from pandas import read_csv
 from unidecode import unidecode
-from sys import exit
+from sys import exit, argv, executable
+from os import execv
 from database import *
 from catraca import update_catraca
 from etiqueta import criar_etiquetas
@@ -9,6 +10,11 @@ from etiqueta import criar_etiquetas
 
 if __name__ == '__main__':
     """Main function"""
+    print("\nSelecione a ação desejada:\n1)Adicionar pessoas(BD, catracas) e gerar etiquetas\n"
+          "2)Somente gerar etiquetas\n")
+    opcao = input()
+    if opcao != '1' and opcao != '2':
+        execv(executable, ['python'] + argv)
     # open dialog to select file
     file = filedialog.askopenfile(mode='r', filetypes=[('csv', '.csv')], initialdir='~/Downloads')
     # read file do a dataframe
@@ -26,21 +32,25 @@ if __name__ == '__main__':
         dfTodo = dfTodo.replace(curso, cursoNorm)
     # remove duplicates inside dataframe
     dfTodo = dfTodo.drop_duplicates(subset='pessoa').drop_duplicates(subset='matricula')
-    for row in dfTodo.itertuples():
-        # find duplicates from dataframe in database
-        isIndb = get_pessoa(row.pessoa, row.matricula)
-        # if there is a duplicate, update cartao
-        if isIndb is not None:
-            #update_pessoa(row.pessoa, row.matricula, row.cartao)
-            print('######################## Atualizado ########################')
-            print('Cartão antigo: %s Cartão Novo: %s Pessoa: %s' % (isIndb[2], row.cartao, row.pessoa))
-            update_catraca("E", row.matricula, row.cartao, row.pessoa)
-        # if it is not duplicate, insert into database
-        else:
-            insert_pessoa(row.pessoa, row.matricula, row.cartao, row.id_curso)
-            print('@@@@@@@@@@@@@@@@@@@@@@@@ Cadastrado @@@@@@@@@@@@@@@@@@@@@@@@')
-            print('Pessoa: %s Cartão: %s ' % (row.pessoa, row.cartao))
-            update_catraca("I", row.matricula, row.cartao, row.pessoa)
-    close_db()
-    criar_etiquetas(dfTodo)
-
+    if opcao == '1':
+        for row in dfTodo.itertuples():
+            # find duplicates from dataframe in database
+            isIndb = get_pessoa(row.pessoa, row.matricula)
+            # if there is a duplicate, update cartao
+            if isIndb is not None:
+                update_pessoa(row.pessoa, row.matricula, row.cartao)
+                print('######################## Atualizado ########################')
+                print('Cartão antigo: %s Cartão Novo: %s Pessoa: %s' % (isIndb[2], row.cartao, row.pessoa))
+                update_catraca("A", row.matricula, row.cartao, row.pessoa)
+            # if it is not duplicate, insert into database
+            else:
+                insert_pessoa(row.pessoa, row.matricula, row.cartao, row.id_curso)
+                print('@@@@@@@@@@@@@@@@@@@@@@@@ Cadastrado @@@@@@@@@@@@@@@@@@@@@@@@')
+                print('Pessoa: %s Cartão: %s ' % (row.pessoa, row.cartao))
+                update_catraca("I", row.matricula, row.cartao, row.pessoa)
+        # close database
+        close_db()
+        # create labels for printing
+        criar_etiquetas(dfTodo)
+    elif opcao == '2':
+        criar_etiquetas(dfTodo)
