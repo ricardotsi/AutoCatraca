ecf95eebf092d5643b826814c16de7b0e6852205
Ricardo Arruda <ricardo.sowek@gmail.com>
2022-08-01 14:08:14 -0300

    delete

--- catraca.py
+++ catraca.py
@@ -65,7 +65,7 @@ def thread(index, evento):
     conn.send(packet_format(evento).encode())
     # print the response
     res = "Catraca "+str(index + 1)+" == "+conn.recv(int(params['buffersize'])).decode()
-    print("Deletado: "+res+"\n")
+    print(evento+" | Resultado: "+res+"\n")
     # close connection
     conn.close()
     return res
